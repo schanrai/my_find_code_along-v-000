b4a7e3769d3ba69bb69eb4268db77ead976f5f0d
Sushi Chanrai <sushi@pathworks.co>
2018-07-03 12:43:27 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,10 +4,10 @@ def my_find(collection)
  i = 0
  while i < collection.length
   if yield(collection[i])
-
     return collection[i]
-    binding.pry
+
  end
+     binding.pry
    i = i+1
  end
 end
